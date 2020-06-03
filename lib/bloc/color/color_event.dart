c12130fedcc12cf4d7b26cae9290cb8fdf901cdc
ThanHtikeKyaw <58647553+ThanHtikeKyaw199715@users.noreply.github.com>
2020-06-03 10:00:32 +0630

    Initial Commit

---
+++ lib/bloc/color/color_event.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+@immutable
+abstract class ColorEvent {}
+
+class ChangeColorEvent extends ColorEvent {
+  final Color color;
+  ChangeColorEvent({this.color});
+}
---
+++ lib/ui/partial/page_two.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+
+class PageTwo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Page Two"),
+      ),
+      body: Container(
+        color: Colors.green,
+        margin: EdgeInsets.all(10.0),
+        child: Center(
+          child: Text(
+            "Page Two",
+            style: TextStyle(
+                color: Colors.white
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
